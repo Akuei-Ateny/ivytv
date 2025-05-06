0f0011c348faa30e36a0fc52151d9a87f5fa51e9
Akuei Akuei <akuei@princeton.edu>
2025-05-06 04:33:56 -0400

    Trying

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -11,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   preview: {
-    host: "ivytv.onrender.com"
+    allowedHosts: ["ivytv.onrender.com"]
   },
   plugins: [
     react(),
